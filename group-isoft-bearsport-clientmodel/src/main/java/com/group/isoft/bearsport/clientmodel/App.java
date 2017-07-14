2c06a36aad21f241ed11d726d5154fc3490f4cc2
AndyYang78 <965998042@qq.com>
2017-07-14 10:24:06 +0800

    Init the project

---
+++ group-isoft-bearsport-clientmodel/src/main/java/com/group/isoft/bearsport/clientmodel/App.java
@@ -0,0 +1,13 @@
+package com.group.isoft.bearsport.clientmodel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
