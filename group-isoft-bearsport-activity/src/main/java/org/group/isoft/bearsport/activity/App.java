8a4406070aea30b4e7deb63c57e876bc0e48b2e9
AndyYang78 <965998042@qq.com>
2017-07-14 10:22:51 +0800

    Init the project

---
+++ group-isoft-bearsport-activity/src/main/java/org/group/isoft/bearsport/activity/App.java
@@ -0,0 +1,13 @@
+package org.group.isoft.bearsport.activity;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
