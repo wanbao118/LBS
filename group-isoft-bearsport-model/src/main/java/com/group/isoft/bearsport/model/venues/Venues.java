accc8653c05ac95f886b8448a3c013be2dbe97b9
AndyYang78 <965998042@qq.com>
2017-07-28 23:14:37 +0800

    add venues model

--- group-isoft-bearsport-model/src/main/java/com/group/isoft/bearsport/model/venues/Venues.java
+++ group-isoft-bearsport-model/src/main/java/com/group/isoft/bearsport/model/venues/Venues.java
@@ -1,5 +1,98 @@
 package com.group.isoft.bearsport.model.venues;
 
 public class Venues {
+	private String id;
+	private String venuesId;
+	private String venuesName;
+	private String longitude;
+	private String latitude;
+	private String address;
+	private String contactNum;
+	private String overallRating;
+	private double price;
+	private String envRating;
+	private String serviceRating;
+	private String detlUrl;
+	private String uid;
 
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getVenuesId() {
+		return venuesId;
+	}
+	public void setVenuesId(String venuesId) {
+		this.venuesId = venuesId;
+	}
+	public String getVenuesName() {
+		return venuesName;
+	}
+	public void setVenuesName(String venuesName) {
+		this.venuesName = venuesName;
+	}
+	public String getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+	public String getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getContactNum() {
+		return contactNum;
+	}
+	public void setContactNum(String contactNum) {
+		this.contactNum = contactNum;
+	}
+	public String getOverallRating() {
+		return overallRating;
+	}
+	public void setOverallRating(String overallRating) {
+		this.overallRating = overallRating;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getEnvRating() {
+		return envRating;
+	}
+	public void setEnvRating(String envRating) {
+		this.envRating = envRating;
+	}
+	public String getServiceRating() {
+		return serviceRating;
+	}
+	public void setServiceRating(String serviceRating) {
+		this.serviceRating = serviceRating;
+	}
+	public String getDetlUrl() {
+		return detlUrl;
+	}
+	public void setDetlUrl(String detlUrl) {
+		this.detlUrl = detlUrl;
+	}
+	public String getUid() {
+		return uid;
+	}
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+
+	
 }
