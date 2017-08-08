f3f6ad1fe25e5302c8669353489aa8ce86ab298e
AndyYang78 <965998042@qq.com>
2017-08-08 17:08:12 +0800

    add activityMapper

---
+++ group-isoft-bearsport-persist/src/main/java/com/group/isoft/bearsport/persist/activity/ActivityMapper.java
@@ -0,0 +1,7 @@
+package com.group.isoft.bearsport.persist.activity;
+
+import com.group.isoft.bearsport.model.activity.Activity;
+
+public interface ActivityMapper {
+	 boolean addActivity(Activity activity);
+}
