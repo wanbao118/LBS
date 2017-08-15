58fa6f73db1220480157b2e827a784ef0ba051c9
AndyYang78 <965998042@qq.com>
2017-08-15 22:36:50 +0800

    add userActivity Model

--- group-isoft-bearsport-model/src/main/java/com/group/isoft/bearsport/model/useractivity/UserActivity.java
+++ group-isoft-bearsport-model/src/main/java/com/group/isoft/bearsport/model/useractivity/UserActivity.java
@@ -1,5 +1,57 @@
 package com.group.isoft.bearsport.model.useractivity;
 
-public class UserActivity {
+import java.util.Date;
 
+public class UserActivity {
+	private String actId;
+	private String userId;
+	private String joinerName;
+	private Date joinDate;
+	private String joinTime;
+	private int status;
+	private String detail;
+
+	public String getActId() {
+		return actId;
+	}
+	public void setActId(String actId) {
+		this.actId = actId;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getJoinerName() {
+		return joinerName;
+	}
+	public void setJoinerName(String joinerName) {
+		this.joinerName = joinerName;
+	}
+	public Date getJoinDate() {
+		return joinDate;
+	}
+	public void setJoinDate(Date joinDate) {
+		this.joinDate = joinDate;
+	}
+	public String getJoinTime() {
+		return joinTime;
+	}
+	public void setJoinTime(String joinTime) {
+		this.joinTime = joinTime;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public String getDetail() {
+		return detail;
+	}
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+	
 }
