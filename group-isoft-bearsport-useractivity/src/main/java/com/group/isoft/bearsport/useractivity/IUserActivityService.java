186fc0a407fd72d1f4784d43c189b2af53c9cf3c
AndyYang78 <965998042@qq.com>
2017-08-18 11:36:40 +0800

    add cancel activity service layer

--- group-isoft-bearsport-useractivity/src/main/java/com/group/isoft/bearsport/useractivity/IUserActivityService.java
+++ group-isoft-bearsport-useractivity/src/main/java/com/group/isoft/bearsport/useractivity/IUserActivityService.java
@@ -5,4 +5,5 @@ import com.group.isoft.bearsport.clientmodel.userActivity.UserActivityRespModel;
 
 public interface IUserActivityService {
 	UserActivityRespModel addUserActivity(UserActivityReqModel userActivityReqModel) throws Exception;
+	UserActivityRespModel deleteUserActivity(UserActivityReqModel userActivityReqModel) throws Exception;
 }
