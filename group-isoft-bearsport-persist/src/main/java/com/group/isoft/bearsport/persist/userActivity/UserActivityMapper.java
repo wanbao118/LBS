17b674f6fe69766a3a619760255a0289677a6d64
AndyYang78 <965998042@qq.com>
2017-08-21 21:56:24 +0800

    update userActivity persist layer

--- group-isoft-bearsport-persist/src/main/java/com/group/isoft/bearsport/persist/userActivity/UserActivityMapper.java
+++ group-isoft-bearsport-persist/src/main/java/com/group/isoft/bearsport/persist/userActivity/UserActivityMapper.java
@@ -1,11 +1,13 @@
 package com.group.isoft.bearsport.persist.userActivity;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.group.isoft.bearsport.model.useractivity.ActivityJoinerInfo;
 import com.group.isoft.bearsport.model.useractivity.UserActivity;
 
 public interface UserActivityMapper {
 	Boolean addUserActivity(UserActivity userActivity);
-	UserActivity fetchUserActivityByUidAndActId(String actId, String userId);
+	UserActivity fetchUserActivityByUidAndActId(@Param("actId")String actId, @Param("userId")String userId);
 	int updateUserActivity(UserActivity userActivity);
 	ActivityJoinerInfo getActivityJoinersbyAcctId(String actId);
 }
