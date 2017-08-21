f03b43593c2de4e2b20ea808bc962380a700a437
AndyYang78 <965998042@qq.com>
2017-08-21 21:56:08 +0800

    Update ActivityController

--- group-isoft-bearsport-controller/src/main/java/com/group/isoft/bearsport/controller/activity/ActivityController.java
+++ group-isoft-bearsport-controller/src/main/java/com/group/isoft/bearsport/controller/activity/ActivityController.java
@@ -1,5 +1,7 @@
 package com.group.isoft.bearsport.controller.activity;
 
+import java.util.Date;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,6 +16,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.group.isoft.bearsport.activity.IActivityService;
 import com.group.isoft.bearsport.clientmodel.activity.ActivityReqModel;
 import com.group.isoft.bearsport.clientmodel.activity.ActivityRespModel;
+import com.group.isoft.bearsport.clientmodel.userActivity.UserActivityReqModel;
+import com.group.isoft.bearsport.clientmodel.userActivity.UserActivityRespModel;
+import com.group.isoft.bearsport.useractivity.IUserActivityService;
+import com.group.isoft.bearsport.util.ErrorCode;
 import com.group.isoft.bearsport.util.OperationCode;
 
 @Controller
@@ -23,6 +29,9 @@ public class ActivityController {
 
 	@Resource
 	IActivityService activityService;
+	
+	@Resource
+	IUserActivityService userActivityService;
 
 	@RequestMapping(value = "/activityMaintain", method = RequestMethod.POST, consumes = "application/json")
 	@ResponseBody
@@ -41,7 +50,31 @@ public class ActivityController {
 	}
 
 	private ActivityRespModel createActivity(@RequestBody ActivityReqModel activityRequest) throws Exception {
-		return activityService.createActivity(activityRequest);
+		ActivityRespModel activityRespModel = new ActivityRespModel();
+		UserActivityRespModel userActivityRespModel = new UserActivityRespModel();
+		UserActivityReqModel userActivityReqModel = new UserActivityReqModel();
+
+		activityRespModel = activityService.createActivity(activityRequest);
+
+		if (activityRespModel.getResult().equalsIgnoreCase(ErrorCode.RESPONSE_SUCCESS)) {
+			userActivityReqModel.setActId(activityRespModel.getParams().get("actId"));
+			userActivityReqModel.setUserId(activityRespModel.getParams().get("userId"));
+
+			userActivityReqModel.setJoinDate(new Date());
+
+			//add field in the DB and pass from FE
+			userActivityReqModel.setJoinerName("");
+			userActivityReqModel.setJoinTime("");
+			userActivityReqModel.setJoinerType("0");
+			userActivityReqModel.setJoinRemark(activityRequest.getWordInput());
+			userActivityRespModel = userActivityService.addUserActivity(userActivityReqModel);
+
+			if (userActivityRespModel.getResult().equalsIgnoreCase(ErrorCode.RESPONSE_ERROR)) {
+				activityRespModel.setResult(ErrorCode.RESPONSE_ERROR);
+			}
+		}
+
+		return activityRespModel;
 	}
 
 	private ActivityRespModel getActivityList(@RequestBody ActivityReqModel activityRequest) throws Exception {
