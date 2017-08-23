83b4ed049b764b9dcc518c19850a3eba4df67c5a
AndyYang78 <965998042@qq.com>
2017-08-23 14:20:45 +0800

    update userActivityRespData

--- group-isoft-bearsport-clientmodel/src/main/java/com/group/isoft/bearsport/clientmodel/userActivity/UserActivityRespData.java
+++ group-isoft-bearsport-clientmodel/src/main/java/com/group/isoft/bearsport/clientmodel/userActivity/UserActivityRespData.java
@@ -1,15 +1,43 @@
 package com.group.isoft.bearsport.clientmodel.userActivity;
 
+import java.util.Date;
+
 public class UserActivityRespData {
+	private String uesrId;
+	private String username;
+	private Date joinDate;
+	private String joinTime;
+	private String remark;
 
-	private ActivityJoinerDetl activityJionerDetl;
-
-	public ActivityJoinerDetl getActivityJionerDetl() {
-		return activityJionerDetl;
+	public String getUesrId() {
+		return uesrId;
+	}
+	public void setUesrId(String uesrId) {
+		this.uesrId = uesrId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public Date getJoinDate() {
+		return joinDate;
+	}
+	public void setJoinDate(Date joinDate) {
+		this.joinDate = joinDate;
+	}
+	public String getJoinTime() {
+		return joinTime;
+	}
+	public void setJoinTime(String joinTime) {
+		this.joinTime = joinTime;
+	}
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
 	}
 
-	public void setActivityJionerDetl(ActivityJoinerDetl activityJionerDetl) {
-		this.activityJionerDetl = activityJionerDetl;
-	}
-	
 }
