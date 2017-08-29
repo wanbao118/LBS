80b7497f65f6aef3cd8c639d762cd185a1a74f7b
AndyYang78 <965998042@qq.com>
2017-08-29 12:00:45 +0800

    update appScerect value

--- group-isoft-bearsport-util/src/main/java/com/group/isoft/bearsport/util/Constant.java
+++ group-isoft-bearsport-util/src/main/java/com/group/isoft/bearsport/util/Constant.java
@@ -2,7 +2,7 @@ package com.group.isoft.bearsport.util;
 
 public class Constant {
 	public static final String APP_ID = "wx924b343e7da09cf6";
-	public static final String SECRET = "e7d652b06d132b54280100de64c23e95";
+	public static final String SECRET = "d8e376bad77552d79e3dfdc317d29856";
 	public static final String GRANT_TYPE = "authorization_code";
 	public static final String OPEN_ID_URL = "https://api.weixin.qq.com/sns/jscode2session";
 	public static final String USER_INFO_URL = "https://api.weixin.qq.com/cgi-bin/userinfo?";
